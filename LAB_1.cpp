3c8cfa4b8a34ed3d07a03930fcbab9b1b1b3eef0
MichalMorun <39159209+MichalMorun@users.noreply.github.com>
2019-03-14 23:33:54 +0100

    Add files via upload

---
+++ LAB_1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <time.h>
+#include "Algorytm_johnsona.h"
+#include <algorithm>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	srand(time(NULL)); // ustawienie początkowe generatora liczb losowych
+
+
+
+	Pobierz_dane();
+	Utworz_dane();
+	Algorytm_Johnsona();
+	Wyswietl();
+	Przeglad_zupelny();
+
+
+	return 0;
+}
+
