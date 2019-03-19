315862cc25633239107ca791d6fb1c1775eb96cc
MichalMorun <39159209+MichalMorun@users.noreply.github.com>
2019-03-19 20:47:42 +0100

    Add files via upload

---
+++ LAB_1/Algorytm_johnsona.h
@@ -0,0 +1,13 @@
+﻿#ifndef AJ
+#define AJ
+
+
+void Pobierz_dane(); // podajemy informacje na temat iloœci maszyn i zadañ
+void Utworz_dane(); // tworzy losowe dane wejœciowe (M,P)
+void Algorytm_Johnsona(); // (M, P)
+void Wyswietl(); // wyswietla dane
+int C_max(); // zwraca wartość cmax
+void Przeglad_zupelny();
+void Permutacje(int [], int, int, int); // do przeglądu
+
+#endif 
